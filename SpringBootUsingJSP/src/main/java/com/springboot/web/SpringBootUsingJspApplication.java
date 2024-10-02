cccf7de52e6d906954eaab8d0c0e2f8e6d7a745f
Madan Shrestha <madan.csmajor@gmail.com>
2024-10-02 13:00:09 -0400

    First Commit

---
+++ SpringBootUsingJSP/src/main/java/com/springboot/web/SpringBootUsingJspApplication.java
@@ -0,0 +1,15 @@
+package com.springboot.web;
+
+
+// Configuring JSP inside the Spring Boot
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUsingJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUsingJspApplication.class, args);
+	}
+
+}
